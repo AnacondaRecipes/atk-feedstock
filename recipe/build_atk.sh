1bfc0f8cee142a8cfecba47154a08dc4cf070537
vchychy <vchychyrko@anaconda.com>
2026-01-16 21:09:34 +0200

    fix: prefix

--- recipe/build_atk.sh
+++ recipe/build_atk.sh
@@ -3,19 +3,19 @@
 set -ex
 
 # # Avoid long shebangs and force build python + build g-ir-scanner
-# echo '#!/usr/bin/env bash' > g-ir-scanner
-# echo "${BUILD_PREFIX}/bin/python ${BUILD_PREFIX}/bin/g-ir-scanner \"\$@\"" >> g-ir-scanner
-# chmod +x ./g-ir-scanner
-# export PATH="${PWD}:${PATH}"
+echo '#!/usr/bin/env bash' > g-ir-scanner
+echo "${PREFIX}/bin/python ${PREFIX}/bin/g-ir-scanner \"\$@\"" >> g-ir-scanner
+chmod +x ./g-ir-scanner
+export PATH="${PWD}:${PATH}"
 
 # Force build tools first (meson will pick g-ir-scanner from PATH)
-export PATH="${BUILD_PREFIX}/bin:${PATH}"
-cat > g-ir-scanner <<'EOF'
-#!/usr/bin/env bash
-exec "${BUILD_PREFIX}/bin/python" "${BUILD_PREFIX}/bin/g-ir-scanner" "$@"
-EOF
-chmod +x g-ir-scanner
-export PATH="${PWD}:${PATH}"
+# export PATH="${BUILD_PREFIX}/bin:${PATH}"
+# cat > g-ir-scanner <<'EOF'
+# #!/usr/bin/env bash
+# exec "${BUILD_PREFIX}/bin/python" "${BUILD_PREFIX}/bin/g-ir-scanner" "$@"
+# EOF
+# chmod +x g-ir-scanner
+# export PATH="${PWD}:${PATH}"
 
 meson setup builddir --prefix="$PREFIX" --libdir=lib -Ddocs=false
 meson configure builddir

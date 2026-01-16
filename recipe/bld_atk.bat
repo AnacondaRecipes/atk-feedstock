c81a2b91258726676eb58b3e2173594a2827a009
vchychy <vchychyrko@anaconda.com>
2026-01-17 01:22:51 +0200

    fix: bld.bat meson

--- recipe/bld_atk.bat
+++ recipe/bld_atk.bat
@@ -1,6 +1,11 @@
 setlocal EnableDelayedExpansion
 @echo on
 
+set "MESON=%BUILD_PREFIX%\Scripts\meson.exe"
+if not exist "%MESON%" (
+  set "MESON=%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson-script.py"
+)
+
 :: set pkg-config path so that host deps can be found
 :: (set as env var so it's used by both meson and during build with g-ir-scanner)
 set "PKG_CONFIG_PATH=%LIBRARY_LIB%\pkgconfig;%LIBRARY_PREFIX%\share\pkgconfig"
@@ -18,13 +23,11 @@ set ^"MESON_OPTIONS=^
   --backend=ninja ^
   -Ddocs=false ^
  ^"
- 
-:: configure build using meson
-%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson setup builddir !MESON_OPTIONS!
+
+%MESON% setup builddir !MESON_OPTIONS!
 if errorlevel 1 exit 1
 
-:: print results of build configuration
-%BUILD_PREFIX%\python.exe %BUILD_PREFIX%\Scripts\meson configure builddir
+%MESON% configure builddir
 if errorlevel 1 exit 1
 
 ninja -v -C builddir -j %CPU_COUNT%
